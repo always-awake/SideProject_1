79fa060488b6f39cbe5e292d951e8b7122e87a87
leemirim <leemirim9654@gmail.com>
2019-03-02 18:34:36 +0700

    Tag 모델 생성(ManyToMany 관계 실습)

--- sideproject/blog/models.py
+++ sideproject/blog/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.conf import settings
+from django.db import models
 
 
 class Post(models.Model):
@@ -11,8 +11,12 @@ class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    tag_set = models.ManyToManyField('Tag', blank=True)
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     message = models.TextField()
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
