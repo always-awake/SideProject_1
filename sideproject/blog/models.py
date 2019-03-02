65fc30e7b6aa3a3d6c7cc811953303a72289a7ea
leemirim <leemirim9654@gmail.com>
2019-03-02 17:47:28 +0700

    Blog 모델의 author_name 필드를 ForeignKey를 이용해 author 필드로 변경

--- sideproject/blog/models.py
+++ sideproject/blog/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.conf import settings
 
 
 class Post(models.Model):
-    author_name = models.CharField(max_length=20)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='posts')
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
