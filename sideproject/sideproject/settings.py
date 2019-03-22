09ce4586f848f41d739fbfe688948d0f795dd17a
leemirim <leemirim9654@gmail.com>
2019-03-23 02:27:24 +0900

    media관련 설정 코드 추가

--- sideproject/sideproject/settings.py
+++ sideproject/sideproject/settings.py
@@ -133,12 +133,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'sideproject', 'static'),
 ]
+# media settings
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # django-debug-toolbar 관련 설정
 INTERNAL_IPS = ['127.0.0.1']
--- sideproject/sideproject/urls.py
+++ sideproject/sideproject/urls.py
@@ -1,14 +1,15 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
-    path('shop/', include('shop.urls')),
-]
-
+    path('shop/', include('shop.urls', namespace='shop')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# DEBUG=True일 경우만 동작, False일 경우 빈 리스트 리턴
 
 if settings.DEBUG:
     import debug_toolbar
