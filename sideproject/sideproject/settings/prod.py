80a284ad4ea6baa129ccd00482640e7ab5300e6e
leemirim <leemirim9654@gmail.com>
2019-03-27 23:25:56 +0900

    배포환경에서의 settings.py 수정 - PostgreSQL DB 설정

--- sideproject/sideproject/settings/prod.py
+++ sideproject/sideproject/settings/prod.py
@@ -1,1 +1,11 @@
 from .common import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'admin12345',
+        'HOST': '127.0.0.1',
+    }
+}
